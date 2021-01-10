0ba967cbb21462f7183ac7b4d61ebbba3113cc82
selfrisingfloor <74417622+selfrisingfloor@users.noreply.github.com>
2021-01-10 17:25:51 +0530

    added fancy alerts

--- offline.js
+++ offline.js
@@ -1,11 +1,12 @@
 ﻿{
-	"version": 1610276408,
+	"version": 1610279717,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"media/typewriter2.ogg",
+		"media/microsoft windows xp error - sound effect (hd) - youtube.ogg",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
